733f857a7941915aea8a4133a011db7c141a323d
Artem_PC <taftoos2@gmail.com>
2022-10-01 12:50:30 +0300

    first commit

---
+++ Lesson1/practice1/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+**Например:**
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+Console.WriteLine("Введите число ");
+int num1 = int.Parse(Console.ReadLine()!);
+int result = num1 * num1;
+Console.WriteLine("Результат "+ result);
+/*
+3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+3 -> Среда
+5 ->  Пятница
+*/
+Console.WriteLine("Введите день недели (1,2,3) ");
+int day = int.Parse(Console.ReadLine()!);
+
+if(day==1){    Console.WriteLine("Понедельник");}
+if(day==2){    Console.WriteLine("Вторник");}
+if(day==3){    Console.WriteLine("Среда");}
+if(day==4){    Console.WriteLine("Четверг");}
+if(day==5){    Console.WriteLine("Пятница");}
+if(day==6){    Console.WriteLine("Суббота");}
+if(day==7){    Console.WriteLine("Воскресенье");}
+if(day<1 || day>7){    Console.WriteLine("нет такого дня");}
---
+++ Lesson1/seminar1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Ctrl + /
+// Напишите программу которая на вход принимает два числа и проверяет,является ли первое число квадратом второго
+Console.WriteLine("Введите пераое число ");
+int num1 = int.Parse(Console.ReadLine()!); //int.Parse -конвертируется число в тип Int
+Console.WriteLine("Введите второе число ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+
+if (Math.Pow(num2, 2) == num1){ //Квадратный корень
+    Console.WriteLine($"{num1}Первое число равно квадратом второго{num2}");
+}
+else
+    Console.WriteLine("Первое число не является квадратом второго");
