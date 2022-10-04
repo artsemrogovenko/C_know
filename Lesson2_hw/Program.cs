4713e50bf6982819a66ae693615b84ab0742e7be
Artem_PC <taftoos2@gmail.com>
2022-10-04 23:25:51 +0300

    Решена задача 13

--- Lesson2_hw/Program.cs
+++ Lesson2_hw/Program.cs
@@ -1,14 +1,35 @@
 ﻿// Задача 10:
+/*
 Console.WriteLine("Введите трехзначное число");
 int number = int.Parse(Console.ReadLine()!);
 
-if (number>99 && number<1000){
+if (number > 99 && number < 1000)
+{
 
-int num1 = number / 100;
-int num3 = number % 10;
-int num2 = number - ((num1 * 100) + num3);
-num2 = num2 / 10;
-Console.WriteLine($"Второе число {num2}");
+    int num1 = number / 100;
+    int num3 = number % 10;
+    int num2 = number - ((num1 * 100) + num3);
+    num2 = num2 / 10;
+    Console.WriteLine($"Второе число {num2}");
 }
 else
     Console.WriteLine("Число не трехзначное");
+*/
+//задача 13
+Console.WriteLine("Программа выводит третью цифру заданного числа");
+Console.WriteLine("Введите число");
+//int number = (int)Math.Pow(1000,3);
+int number = int.Parse(Console.ReadLine()!);
+if (number > 99 || number < -99){
+    while (number > 999 || number < -999)    {
+        number = number / 10;
+        Console.WriteLine(number);
+    }
+    if (number > 99 && number < 1000 || number < -99 && number > -1000)
+    {
+        int num3 = number % 10;
+        Console.WriteLine($"Третья цифра {num3}");
+    }
+}
+else
+    Console.WriteLine("третьей цифры нет");
