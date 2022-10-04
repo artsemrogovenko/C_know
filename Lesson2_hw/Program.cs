c22a929cd9cb136bb9f67218a707bda85630ecd3
Artem_PC <taftoos2@gmail.com>
2022-10-04 21:53:38 +0300

    Решена задача 10

--- Lesson2_hw/Program.cs
+++ Lesson2_hw/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10:
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number>99 && number<1000){
+
+int num1 = number / 100;
+int num3 = number % 10;
+int num2 = number - ((num1 * 100) + num3);
+num2 = num2 / 10;
+Console.WriteLine($"Второе число {num2}");
+}
+else
+    Console.WriteLine("Число не трехзначное");
